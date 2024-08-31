1dfad85443384d4cdef6f03c2287679558216d8e
Raul <80535950+hideNFN@users.noreply.github.com>
2024-08-31 03:39:33 +0300

    Update youtubedownloader.py

--- master/youtubedownloader.py
+++ master/youtubedownloader.py
@@ -1,32 +1,38 @@
 import os
-from pytube import YouTube
+from pytube import YouTube, exceptions
 
 print("YouTube Downloader v1.0 by hideNFN\n")
 
-verificarefolder = os.getcwd() + "\\.files"
-if os.path.exists(verificarefolder) is False:
+verificarefolder = os.path.join(os.getcwd(), ".files")
+if not os.path.exists(verificarefolder):
     print("Creating necessary folder...")
     os.mkdir(verificarefolder)
 
 def mp4dwn():
     while True:
-        
         link = input("\nPaste the URL of the video you would like to download:\n")
 
-        print("\nBeginning download...")
+        video_downloaded = False
 
         try:
             yt = YouTube(link)
-            yt.streams.filter(adaptive = True, file_extension = "mp4").first().download(max_retries = 2, output_path=".files")
-            print( "\nVideo file was downloaded successfully.")
-        except:
-            print("\nError, the video file download failed.")
+            print("\nBeginning video download...")
+            yt.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution().download(output_path=".files")
+            print("\nVideo file was downloaded successfully.")
+            video_downloaded = True
+        except exceptions.VideoUnavailable:
+            print("\nError: The video is unavailable.")
+        except Exception as e:
+            print(f"\nError, the video file download failed: {e}")
 
-        try:
-            yt = YouTube(link)
-            yt.streams.filter(only_audio = True).first().download(filename_prefix = "audio", max_retries = 2, output_path=".files")
-            print("\nAudio file was downloaded successfully.")
-        except:
-            print("\nError, the audio file download failed.")
+        if video_downloaded:
+            try:
+                print("\nBeginning audio download...")
+                yt.streams.filter(only_audio=True).first().download(output_path=".files", filename_prefix="audio")
+                print("\nAudio file was downloaded successfully.")
+            except Exception as e:
+                print(f"\nError, the audio file download failed: {e}")
+        else:
+            print("\nSkipping audio download due to video download failure.")
 
 mp4dwn()
